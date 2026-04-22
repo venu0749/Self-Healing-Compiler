2de9dbc6575000232304c5144577e69ff1945125
venu0749 <vr24csb0a84@student.nitw.ac.in>
2026-04-22 11:40:48 +0530

    Delete src directory

--- src/ASTNode.java
+++
@@ -1,5 +0,0 @@
-public abstract class ASTNode {
-    public abstract boolean isValid();
-    public abstract String tostring();//for IR visualization
-}
-//now AST is printable  means this becomes visible IRs
--- src/Verifier.java
+++
@@ -1,6 +0,0 @@
-public class Verifier {
-
-    public static boolean verify(String code) {
-        return code.contains(";") && !code.contains("gets(");
-    }
-}
